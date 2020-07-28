36618f318e4ab09765c49392e214ace43fbd359a
bagus.yanuar613 <bagus.yanuar613@gmail.com>
2020-07-29 00:12:01 +0700

    enum

---
+++ app/Http/Controllers/Main/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Main;
+
+use App\Helper\CustomController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainController extends CustomController
+{
+    //
+
+    /**
+     * MainController constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+
+    }
+}
---
+++ database/migrations/2020_07_28_171100_enum.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Enum extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->enum('status', ['menunggu', 'terima', 'pinjam', 'tolak', 'kembali']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            //
+        });
+    }
+}
