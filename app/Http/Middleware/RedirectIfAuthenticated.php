38ca1e2a7abea5a659a9a5dd251c598622a82491
bagus.yanuar613 <bagus.yanuar613@gmail.com>
2020-07-29 16:11:30 +0700

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,12 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            if (auth()->user()->roles == 'admin'){
+                return redirect('/admin');
+                dd(auth()->user()->roles);
+            }
+            dd(auth()->user()->roles);
+            return redirect('/');
         }
 
         return $next($request);
